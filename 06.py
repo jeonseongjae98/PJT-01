9a9d5e120752c48bb3837b9b7aca089d5cdc5571
kdt-live <108608439+kdt-live@users.noreply.github.com>
2023-01-06 11:05:01 +0900

    Update 06.py

--- 06.py
+++ 06.py
@@ -1,10 +1,7 @@
 import json
 
 # 아래 코드 수정 금지
-movie_json = open("data/movie.json", encoding="UTF8")
-movie = json.load(movie_json)
+movies_json = open("data/movies.json", encoding="UTF8")
+movies_list = json.load(movies_json)
 
-genres_json = open("data/genres.json", encoding="UTF8")
-genres_list = json.load(genres_json)
-
-# 이하 문제 해결을 위한 코드 작성 
+# 이하 문제 해결을 위한 코드 작성
